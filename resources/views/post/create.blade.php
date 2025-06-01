bce3e6bb55780c974acfbd17311051d71ddd7164
EJD222 <eljdiaz500@gmail.com>
2025-06-01 15:21:33 +0800

    feat: implement post creation with model, controller, and form

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,56 @@
+<x-app-layout>
+    <div class="py-8">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-8">
+                <form action="{{ route('post.store') }}" enctype="multipart/form-data" method="post">
+                    @csrf
+
+                    <!-- Category -->
+                    <div class="mt-4">
+                        <x-input-label for="category_id" :value="__('Content')" />
+                        <select id="category_id" name="category_id"
+                            class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
+                            <option value="">Select a Category</option>
+                            @foreach($categories as $category)
+                                <option value={{ $category->id }} @selected(old('category_id') == $category->id)> {{ $category->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <x-input-error :messages="$errors->get('category_id')" class="mt-2" />
+                    </div>
+
+                    <!-- Image -->
+                    <div class="mt-4">
+                        <x-input-label for="image" :value="__('Image')" />
+                        <input id="image" class="block mt-1 w-full" name="image"
+                            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                            aria-describedby="file_input_help" id="image" type="file">
+                        <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="image">SVG, PNG, JPG or
+                            GIF (MAX. 800x400px).</p>
+                        <x-input-error :messages="$errors->get('image')" class="mt-2" />
+                    </div>
+
+                    <!-- Title -->
+                    <div class="mt-4">
+                        <x-input-label for="title" :value="__('Title')" />
+                        <x-text-input id="title" class="block mt-1 w-full" type="text" name="title"
+                            :value="old('title')" autofocus />
+                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                    </div>
+
+                    <!-- Content -->
+                    <div class="mt-4">
+                        <x-input-label for="content" :value="__('Content')" />
+                        <x-text-area id="content" class="block mt-1 w-full" type="text" name="content">
+                            {{ old('content') }}
+                        </x-text-area>
+                        <x-input-error :messages="$errors->get('content')" class="mt-2" />
+                    </div>
+
+                    <x-primary-button class="mt-4">
+                        Submit
+                    </x-primary-button>
+                </form>
+            </div>
+        </div>
+</x-app-layout>
