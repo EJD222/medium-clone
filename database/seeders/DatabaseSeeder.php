c36ac09903399118cf32f6fcdede6f672f3c19b4
EJD222 <eljdiaz500@gmail.com>
2025-06-03 18:21:00 +0800

    feat: add username, bio, and image fields to user profile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
+            'username' => 'test-user',
             'email' => 'test@example.com'
         ]);
         
@@ -34,6 +35,6 @@ class DatabaseSeeder extends Seeder
             Category::create(['name' => $category]);
         }
 
-        Post::factory(100)->create();
+        //Post::factory(100)->create();
     }
 }
