4af24177026727ca5742bf57cc7233ea0ba43313
EJD222 <eljdiaz500@gmail.com>
2025-06-03 18:23:25 +0800

    refactor: move post creation validation to PostCreateRequest

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostCreateRequest;
+use App\Http\Requests\PostCreationRequest;
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -36,15 +38,9 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PostCreateRequest $request)
     {
-        $data = $request -> validate([
-            'image' => ['required','image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
-            'title'=> 'required',
-            'content' => 'required',
-            'category_id' => ['required', 'exists:categories,id'],
-            'published_at' => ['nullable', 'datetime']
-        ]);
+        $data = $request->validated();
 
         $image = $data['image'];
         unset($data['image']);
