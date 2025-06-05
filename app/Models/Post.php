8a9923467d91ed7b10a0cf10395e28ba96515898
EJD222 <eljdiaz500@gmail.com>
2025-06-05 11:36:55 +0800

    feat: add relationships and helper methods to Post and User models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
@@ -18,4 +19,27 @@ class Post extends Model
         'user_id',
         'published_at'
     ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function readTime() {
+        $wordcount = str_word_count(strip_tags($this->content));
+        $minutes = ceil($wordcount /120);
+
+        return max(1,$minutes);
+    }
+
+    public function imageURL() {
+        if($this->image) 
+        {
+            return Storage::url($this->image);   
+        }
+        return null;
+    }
 }
