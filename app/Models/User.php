c7ef95f619d86bfa8d7915f7a8dcf78b9aee4c9b
EJD222 <eljdiaz500@gmail.com>
2025-06-06 16:19:07 +0800

    feat: establish user-post relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Post;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -48,6 +49,10 @@ class User extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function posts() {
+        return $this->hasMany(Post::class);
     }
 
     public function imageURL() {
