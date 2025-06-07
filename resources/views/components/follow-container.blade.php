50d1d60eeafd283214ee165a505e97d4a595218e
EJD222 <eljdiaz500@gmail.com>
2025-06-07 12:13:00 +0800

    bug: prevent follow toggle crash when not logged in and fix async toggle state

--- resources/views/components/follow-container.blade.php
+++ resources/views/components/follow-container.blade.php
@@ -1,13 +1,13 @@
 @props(['user'])
 
 <div {{$attributes}} x-data=" {
-    following: {{ $user->isFollowedBy(auth()->user()) ? 'true' : 'false' }},
+    following: {{  auth()->check() && $user->isFollowedBy(auth()->user()) ? 'true' : 'false' }},
     followersCount: {{$user->followers->count()}},
     follow() {
-        this.following = !this.following
         axios.post('/follow/{{ $user->id }}')
             .then(res=>{
                 console.log(res.data)
+                this.following = !this.following
                 this.followersCount = res.data.followersCount
             })
             .catch(err=> {
@@ -15,5 +15,5 @@
             })
     }
 }" class="w-[320px] border-l px-8">
-{{ $slot }}
+    {{ $slot }}
 </div>
